4de550c909a3eb4ba52c4cefd644b2ac8794dba9
LTianRahul <134691760+LTianRahul@users.noreply.github.com>
2025-04-15 19:10:38 +0530

    Update single.conf.js

--- android/conf/single.conf.js
+++ android/conf/single.conf.js
@@ -1,12 +1,12 @@
-USERNAME =  process.env.BROWSERSTACK_USERNAME || 'rahulkumarlambdatest',
-ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY || 'CfViCjAWKL4e0nMDjzQgmx4ZeCHaZeycxgw4xaakQlApOJxMHQ',
+USERNAME =  '{LT_USERNAME}',
+ACCESS_KEY = '{LT_ACCESS_KEY}',
   
 exports.capabilities = {
     "platformName" : "android",
     "browserName" : "chrome",
     "LT:Options": {
-		"username": "rahulkumarlambdatest",
-		"accessKey": "CfViCjAWKL4e0nMDjzQgmx4ZeCHaZeycxgw4xaakQlApOJxMHQ",
+		"username": "{LT_USERNAME}",
+		"accessKey": "{LT_ACCESS_KEY",
 		"w3c": true,
 		"plugin": "node_js-mocha",
         "platformName" : "android",
